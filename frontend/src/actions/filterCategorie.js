5deae8511808242fb17034942f648b1a63e07d8d
fernandoschulzz <64163131+fernandoschulzz@users.noreply.github.com>
2021-09-07 11:23:41 -0300

    cambio en reducer acction filtercategories

--- frontend/src/actions/filterCategorie.js
+++ frontend/src/actions/filterCategorie.js
@@ -1,6 +1,22 @@
+import axios from "axios";
+
 export function filterByCategories(payload) {
-  return {
-    type: "FILTER_CATEGORIE",
-    payload,
+  return async function (dispatch) {
+    try {
+      let response = await axios.get("http://localhost:8001/nfts");
+      const filterCat =
+      payload === 'all'
+        ? response.data
+        : response.data.filter((i) => i.dappSlug === payload);
+        console.log(payload);
+        console.log(filterCat);
+      return dispatch({
+        type: "FILTER_CATEGORIE",
+        payload: filterCat,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
-}
+};
+
