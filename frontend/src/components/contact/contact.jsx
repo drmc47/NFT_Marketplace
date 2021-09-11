86663979d94ed251ed45b130a769d4ebac3c9cb7
anaharri <anaharringtonb@gmail.com>
2021-09-11 03:02:04 -0300

    componente Contact con Email js

--- frontend/src/components/contact/contact.jsx
+++ frontend/src/components/contact/contact.jsx
@@ -1,6 +1,68 @@
-export default function Contact() {
+import { Button, TextField } from '@material-ui/core'
+import Box from '@material-ui/core/Box'
+import emailjs from 'emailjs-com'
 
-    return(
-        <h3>contact</h3>
-    )
+export default function Contact() {
+  function sendEmail(e) {
+    e.preventDefault()
+    emailjs
+      .sendForm(
+        'contact_service',
+        'contact_form',
+        e.target,
+        'user_N0Deo4uHEueiZVnuDraRQ'
+      )
+      .then(
+        (result) => {
+          console.log(result.text)
+        },
+        (error) => {
+          console.log(error.text)
+        }
+      )
+  }
+  return (
+    <div>
+      <Box
+        display='flex'
+        flexDirection='column'
+        justifyContent='center'
+        alignItems='center'
+      >
+        <h3>Contact us!</h3>
+        <form onSubmit={sendEmail}>
+          <Box mb='1em'>
+            <TextField
+              name='name'
+              type='text'
+              variant='outlined'
+              label='Name'
+            />
+          </Box>
+          <Box mb='1rem'>
+            <TextField
+              name='email'
+              type='email'
+              variant='outlined'
+              label='Email'
+            />
+          </Box>
+          <Box mb='1rem'>
+            <TextField
+              name='message'
+              type='text'
+              variant='outlined'
+              minRows='6'
+              label='Message'
+            />
+          </Box>
+          <Box mb='1rem'>
+            <Button type='submit' variant='outlined' size='large' fullWidth>
+              Send
+            </Button>
+          </Box>
+        </form>
+      </Box>
+    </div>
+  )
 }
