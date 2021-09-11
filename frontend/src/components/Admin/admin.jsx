3faea81b364019b6170ace73238eeca91f892b0a
Sofia <sofiabeleninchausti@gmail.com>
2021-09-11 12:10:01 -0300

    post category

--- frontend/src/components/Admin/admin.jsx
+++ frontend/src/components/Admin/admin.jsx
@@ -26,7 +26,7 @@ export default function AdminProfile() {
       [e.target.name]: e.target.value
     })
   }
-  //algo
+  //ala
   async function handleSubmit(e) {
      e.preventDefault()
      console.log("cat",categories)
