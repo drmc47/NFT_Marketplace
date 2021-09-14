97bbd56a02cb8311f87699367acb0fcca18febd3
¨hbonavota¨ <hbonavota@gmail.com>
2021-09-15 00:58:44 +0200

    solving merge

--- frontend/src/actions/login.js
+++ frontend/src/actions/login.js
@@ -3,16 +3,9 @@ import loginUser from "../services/auth/loginUser";
 export default function localLogin(payload) {
     return async function (dispatch) {
     const response = await loginUser(payload);
-<<<<<<< HEAD
-    if (response) {
-      const user = response;
-      window.sessionStorage.setItem("userLogged", JSON.stringify(user))
-      dispatch({
-=======
     console.log(response,"responseeee")
       if (response) {
         dispatch({
->>>>>>> 4a04d769c1cbebd99cb458474012ae7c84d6e47f
         type: "LOGIN_SUCCESS",
         payload: response,
       });
