d16053aaf33b5d11162baf2523601940be2ab7c2
Agustina Corbalan <tinicorb@gmail.com>
2021-09-17 20:33:43 -0300

    MultiplesCategories

--- frontend/src/actions/filtercategory.js
+++ frontend/src/actions/filtercategory.js
@@ -4,8 +4,10 @@ export function filterByCategories(payload) {
   return async function (dispatch) {
     try {
       let response = await axios.get("http://localhost:8001/nfts");
-      const filterCat = await response.data.filter((i) => i.categories[0] === payload);
-        console.log(payload);
+      const filterCat = await response.data.filter((i) =>
+        i.categories.includes(payload)
+      );
+      console.log(filterCat, "FILTEEER");
       return dispatch({
         type: "FILTER_BY_CATEGORY",
         payload: filterCat,
@@ -14,5 +16,15 @@ export function filterByCategories(payload) {
       console.log(error);
     }
   };
-};
+}
 
+// if(action.payload!=="all"){
+//                 for(let i = 0; i<arr.length; i++) {
+//                     if(arr[i].temperaments){
+//                        if(arr[i].temperaments.filter(e => e.toLowerCase().includes(action.payload.toLowerCase()) === true ).length > 0) {
+//                         aux.push(arr[i])
+//                    }
+//                 }
+//             }
+
+//         }
