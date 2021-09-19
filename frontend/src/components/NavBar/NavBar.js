a2e1ae4b8a65549db7fa3cf21569e01c56d8d3e0
Fernando Schulz <fernandoschulz@MacBook-Air-de-Fernando.local>
2021-09-18 22:43:43 -0300

    agregado de NavBar responsive para celular

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -21,7 +21,19 @@ import { getCategories } from '../../actions/getCategories'
 import { userSession } from '../../actions/userSession'
 import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined'
 import ExitToAppIcon from '@material-ui/icons/ExitToApp'
-// import { createChainedFunction } from '@material-ui/core'
+import { SwipeableDrawer } from '@material-ui/core'
+import MenuIcon from "@material-ui/icons/Menu"
+import { List, ListItem, ListItemText } from '@material-ui/core'
+import HomeIcon from '@material-ui/icons/Home';
+import CategoryIcon from '@material-ui/icons/Category';
+import ContactMailIcon from '@material-ui/icons/ContactMail'
+import InfoIcon from "@material-ui/icons/Info";
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import CreateIcon from '@material-ui/icons/Create';
+import LockOpenIcon from '@material-ui/icons/LockOpen';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
 
 function ElevationScroll(props) {
   const { children } = props
@@ -78,6 +90,33 @@ const useStyles = makeStyles((theme) => ({
   profileMenu: {
     marginTop: '2.6rem',
   },
+  drawerIconContainer: {
+    marginLeft: "auto",
+    "&:hover":  {
+      backgroundColor: "transparent"
+    }
+  },
+  drawerIcon: {
+    height: "40px",
+    width: "40px",
+    color: "white"
+  },
+  drawer: {
+    backgroundColor: theme.palette.secondary.main
+  },
+  drawerText: {
+    ...theme.typography.tab,
+    color: "white",
+    opacity: 0.7
+  },
+  drawerTextSelected: {
+    "& .MuiListItemText-root": {
+      opacity: 1
+    }
+  },
+  loginbutton: {
+    backgroundColor: theme.palette.primary.main
+  }
 }))
 
 export default function NavBar() {
@@ -88,6 +127,9 @@ export default function NavBar() {
   const numberOfItems = number.length
   const classes = useStyles()
   const theme = useTheme()
+  const iOS = typeof navigator !== 'undefined' && /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+  const [openDrawer, setopenDrawer] = useState(false)
   const matches = useMediaQuery(theme.breakpoints.down('sm'))
   const [value, setValue] = useState(0)
   const [anchorEl, setanchorEl] = useState(null)
@@ -96,8 +138,8 @@ export default function NavBar() {
   const [openProfile, setopenProfile] = useState(false)
   const role=useSelector((state) => state.role)
 
-  const handleChange = (e, value) => {
-    setValue(value)
+  const handleChange = (e, newValue) => {
+    setValue(newValue)
   }
 
   const handleclick = (e) => {
@@ -138,10 +180,12 @@ export default function NavBar() {
       setValue(2)
     } else if (window.location.pathname === '/about' && value !== 3) {
       setValue(3)
-    } else if (window.location.pathname === '/create' && value !== 4) {
+    } else if (window.location.pathname === '/admin' && value !== 4) {
       setValue(4)
-    } else if (window.location.pathname === '/profile' && value !== 5) {
+    } else if (window.location.pathname === '/create' && value !== 5) {
       setValue(5)
+    } else if (window.location.pathname === '/login' && value !== 6) {
+      setValue(6)
     }
 
     dispatch(userSession())
@@ -362,6 +406,124 @@ export default function NavBar() {
     </React.Fragment>
   )
 
+  const drawer = (
+    <React.Fragment>
+      <SwipeableDrawer anchor="right" disableBackdropTransition={!iOS} disableDiscovery={iOS} open={openDrawer} 
+      onClose={()=> setopenDrawer(false)} onOpen={()=> setopenDrawer(true)}
+      classes={{paper: classes.drawer}}>
+        <List disablePadding>
+          <ListItem onClick={()=> {setopenDrawer(false); setValue(0)}}
+          selected={value === 0} classes={{selected: classes.drawerTextSelected}}
+          divider button component={Link} to="/"> <HomeIcon color="primary"/>
+            <ListItemText className={classes.drawerText} divider button disableTypography>Home</ListItemText>
+          </ListItem>
+          <ListItem onClick={()=> {setopen(!open); setValue(1)}}
+          selected={value === 1} classes={{selected: classes.drawerTextSelected}}
+          divider button > <CategoryIcon color="primary"/>
+            <ListItemText className={classes.drawerText} disableTypography>Categories </ListItemText>
+            {open ? <ExpandLess /> : <ExpandMore />}
+          </ListItem>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItem onClick={()=> {setopenDrawer(false); setValue(1)}}
+          selected={value === 1}
+          divider button component={Link} to="/categories/all">
+            <ListItemText className={classes.drawerText} disableTypography>All NFTs</ListItemText>
+          </ListItem>
+          {categories.length> 0 && (
+            <ListItem onClick={()=> {setopenDrawer(false); setValue(1)}}
+            selected={value === 1}
+            divider button component={Link} to={`/categories/${categories[0]._id}`}>
+              <ListItemText className={classes.drawerText} disableTypography>{categories[0].name}</ListItemText>
+            </ListItem>
+          )}
+          {categories.length> 1 && (
+            <ListItem onClick={()=> {setopenDrawer(false); setValue(1)}}
+            selected={value === 1}
+            divider button component={Link} to={`/categories/${categories[1]._id}`}>
+              <ListItemText className={classes.drawerText} disableTypography>{categories[1].name}</ListItemText>
+            </ListItem>
+          )}
+          {categories.length> 2 && (
+            <ListItem onClick={()=> {setopenDrawer(false); setValue(1)}}
+            selected={value === 1}
+            divider button component={Link} to={`/categories/${categories[2]._id}`}>
+              <ListItemText className={classes.drawerText} disableTypography>{categories[2].name}</ListItemText>
+            </ListItem>
+          )}
+          {categories.length> 3 && (
+            <ListItem onClick={()=> {setopenDrawer(false); setValue(1)}}
+            selected={value === 1}
+            divider button component={Link} to={`/categories/${categories[3]._id}`}>
+              <ListItemText className={classes.drawerText} disableTypography>{categories[3].name}</ListItemText>
+            </ListItem>
+          )}
+          {categories.length> 4 && (
+            <ListItem onClick={()=> {setopenDrawer(false); setValue(1)}}
+            selected={value === 1}
+            divider button component={Link} to={`/categories/${categories[4]._id}`}>
+              <ListItemText className={classes.drawerText} disableTypography>{categories[4].name}</ListItemText>
+            </ListItem>
+          )}
+          {categories.length> 0 && (
+            <ListItem onClick={()=> {setopenDrawer(false); setValue(1)}}
+            selected={value === 1}
+            divider button component={Link} to="/categories">
+              <ListItemText className={classes.drawerText} disableTypography>Show more...</ListItemText>
+            </ListItem>
+          )}
+        </List>
+      </Collapse>
+          <ListItem onClick={()=> {setopenDrawer(false); setValue(2)}}
+          selected={value === 2} classes={{selected: classes.drawerTextSelected}}
+          divider button component={Link} to="/contact"><ContactMailIcon color="primary"/>
+            <ListItemText className={classes.drawerText} disableTypography>Contact</ListItemText>
+          </ListItem>
+          <ListItem onClick={()=> {setopenDrawer(false); setValue(3)}}
+          selected={value === 3} classes={{selected: classes.drawerTextSelected}}
+          divider button component={Link} to="/about"> <InfoIcon color="primary"/>
+            <ListItemText className={classes.drawerText} disableTypography>About us</ListItemText>
+          </ListItem>
+          {role ==="admin" && (
+          <ListItem onClick={()=> {setopenDrawer(false); setValue(4)}} 
+          selected={value === 4} classes={{selected: classes.drawerTextSelected}}
+          divider button component={Link} to="/admin"> <SupervisorAccountIcon color="primary"/>
+            <ListItemText className={classes.drawerText} disableTypography>Admin</ListItemText>
+          </ListItem>)}
+          {userLogged && (
+          <ListItem onClick={()=> {setopenDrawer(false); setValue(5)}} 
+          selected={value === 5} classes={{selected: classes.drawerTextSelected}}
+          divider button component={Link} to="/create"> <CreateIcon color="primary"/>
+            <ListItemText className={classes.drawerText} disableTypography>Create</ListItemText>
+          </ListItem>
+          )}
+          {userLogged && (
+          <ListItem onClick={()=> {setopenDrawer(false); setValue(6)}} 
+          selected={value === 6} classes={{selected: classes.drawerTextSelected}}
+          divider button component={Link} to="/profile"> <AccountCircleOutlinedIcon color="primary"/>
+            <ListItemText className={classes.drawerText} disableTypography>My Profile</ListItemText>
+          </ListItem>
+          )}
+          {userLogged ? 
+           <ListItem className={classes.loginbutton}
+           classes={{selected: classes.drawerTextSelected}}
+            onClick={()=> {setopenDrawer(false); setValue(0); handleLogout()}} divider button component={Link} to="/"><ExitToAppIcon color="error"/>
+             <ListItemText className={classes.drawerText} disableTypography>Logout</ListItemText>
+           </ListItem> :
+          <ListItem className={classes.loginbutton}
+          selected={value === 6} classes={{selected: classes.drawerTextSelected}}
+           onClick={()=> {setopenDrawer(false); setValue(6)}} divider button component={Link} to="/login"><LockOpenIcon color="error"/>
+            <ListItemText className={classes.drawerText} disableTypography>Login</ListItemText>
+          </ListItem>}
+        </List>
+        </SwipeableDrawer>
+        <IconButton className={classes.drawerIconContainer} 
+        onClick={()=> setopenDrawer(!openDrawer)} disableRipple>
+          <MenuIcon className={classes.drawerIcon} />
+        </IconButton>
+    </React.Fragment>
+  )
+
   return (
     <React.Fragment>
       <ElevationScroll>
@@ -370,7 +532,7 @@ export default function NavBar() {
             <Typography color='white' variant='h5'>
               NFT MARKET
             </Typography>
-            {matches ? null : tabs}
+            {matches ? drawer : tabs}
           </ToolBar>
         </AppBar>
       </ElevationScroll>
