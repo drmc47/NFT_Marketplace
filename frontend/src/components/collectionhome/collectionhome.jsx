35ceeec87b1c5b9991f4a11e7688248ee391b389
anaharri <anaharringtonb@gmail.com>
2021-09-20 12:51:51 -0300

    estilos formulario create

--- frontend/src/components/collectionhome/collectionhome.jsx
+++ frontend/src/components/collectionhome/collectionhome.jsx
@@ -1,9 +1,9 @@
-import Cards from "../card/card.jsx"
-import Grid from '@material-ui/core/Grid';
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getNFTs } from "../../actions/getNFTs.js";
-import style from "../collectionhome/collectionhome.module.css";
+import Cards from '../card/card.jsx'
+import Grid from '@material-ui/core/Grid'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getNFTs } from '../../actions/getNFTs.js'
+import style from '../collectionhome/collectionhome.module.css'
 import { Link } from 'react-router-dom'
 import { makeStyles } from '@material-ui/styles'
 
@@ -13,35 +13,28 @@ import { makeStyles } from '@material-ui/styles'
 //     },
 // }))
 
-
 export default function ColectionHome() {
+  const stateAllNFTs = useSelector((state) => state.allNFTs)
+  const selected = stateAllNFTs.slice(14, 20)
+  const dispatch = useDispatch()
 
-    const stateAllNFTs = useSelector((state) => state.allNFTs);
-const selected = stateAllNFTs.slice(14,20)
-const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getNFTs())
+  }, [dispatch])
 
-useEffect(() => {
-    dispatch(getNFTs());
-  }, [dispatch]);
+  return (
+    <div className={style.houselists}>
+      {selected.length > 0
+        ? selected.map((ele) => (
+            <Link to={`nft/${ele._id}`} style={{ textDecoration: 'none' }}>
+              <div className={style.house}>
+                <img src={ele.image} className={style.image} />
 
-
-    return(
-        <div className={style.houselists}>
-            {selected. length > 0 ?(
-                selected.map((ele) => (
-                    <Link to={`nft/${ele._id}`} style={{textDecoration: "none"}}>
-                    <div className={style.house} >
-                         
-                        <img src={ele.image} className={style.image}/>
-                        
-                        <div class={style.house_name}>{ele.name.slice(0,17)}</div>
-                       
-                    </div>
-                    </Link>
-                    
-                   
-                ))
-            ): null}
-        </div>
-    )
+                <div class={style.house_name}>{ele.name.slice(0, 17)}</div>
+              </div>
+            </Link>
+          ))
+        : null}
+    </div>
+  )
 }
